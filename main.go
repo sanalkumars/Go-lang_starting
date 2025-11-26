f0febccf0d401571a8b052ee6291b628afceea71
sanalkumars <sanalskumar126@gmail.com>
2025-11-26 17:21:45 +0530

    variables done

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello, World!")
+
+    a :=8;
+    var hai string ="sanal"
+    var A,b,c,d int = 1,2,3,5
+    fmt.Println("a",a)
+    fmt.Println("hai",hai)
+    fmt.Println("multiple vars",A,b,c,d)
+
+    // calling the array in the main.go file for running that to
+    arrys()
 }
