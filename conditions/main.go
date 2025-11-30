d574eb5e23f9ad45e043f65532b68825453d789f
sanalkumars <sanalskumar126@gmail.com>
2025-11-30 15:32:42 +0530

    conditional statements done

---
+++ conditions/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// age := 68
+
+	// if age > 20 {
+	// 	fmt.Println("adult")
+	// }
+	
+	// if age > 20 && age < 60 {
+	// 	fmt.Println("adult")
+	// } else if age > 60 {
+	// 	fmt.Println("senior adult")
+	// }else{
+	// 	fmt.Println("minor")
+	// }
+
+	// switch statement
+	day :="monday"
+
+	switch day {
+	case "monday":
+		fmt.Println("the day is monday")
+	case "tuesday":
+		fmt.Println("day is tuesday")
+	}
+	
+}
