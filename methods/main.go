9bfb213cbc850b69bbff8b2e746ccbaae215fd33
sanalkumars <sanalskumar126@gmail.com>
2025-12-01 16:35:34 +0530

    method + pointer + structure manipulation done

---
+++ methods/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Address struct{
+	House_No int
+	City string
+}
+
+type User struct {
+	Name    string
+	Age     int
+	Address Address
+	Status string
+}
+
+func main() {
+	fmt.Print("structure in go lang \n")
+
+	sanal := User{ Name: "Sanal",Age: 23,Address: Address{City: "halo",House_No: 212},Status: "active"}
+	sanal.getUserStatus()
+	sanal.changeStatus()
+	sanal.changeStatusPermenent()
+	sanal.getUserStatus()
+
+
+}
+
+func (u User)getUserStatus()  {
+	fmt.Println("user status is ",u.Status)
+}
+
+// funcation for manupulating the values for the created object/structure
+
+
+func (u User)changeStatus()  {
+	u.Status ="inactive" // this change is only temporary, as we are only passing the copy of the original object
+	fmt.Println("new status is ",u.Status) 
+}
+// here instead of the copy we are passing a pointer to the original object
+func (u *User)changeStatusPermenent()  {
+	u.Status ="pending" // this change is only temporary, as we are only passing the copy of the original object
+	fmt.Println("new status is ",u.Status) 
+}
